7dd7a6bc533e031ad744abb8faffb377f7c4d758
egregiouss <egorushka645258@gmail.com>
2021-11-08 03:16:24 +0500

    add player, movemment

--- main.py
+++ main.py
@@ -1,26 +1,39 @@
 import sys
-
-import pygame
-
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-clock = pygame.time.Clock()
+from player import Player
+import pygame as pg
 
 
-def main():
-    pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    while True:
-        for e in pygame.event.get():
-            if e.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
+class Game:
+    def __init__(self):
+        self.player = pg.sprite.GroupSingle(Player((SCREEN_WIDTH /2, SCREEN_HEIGHT)))
 
-            screen.fill((0, 0, 0))
-            pygame.display.flip()
-            clock.tick(60)
+    def update(self):
+        self.player.update()
+        self.player.draw(screen)
+
+
+
+
+
 
 
 if __name__ == "__main__":
-    main()
+    pg.init()
+    SCREEN_WIDTH = 600
+    SCREEN_HEIGHT = 600
+    screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pg.time.Clock()
+    game = Game()
+
+    while True:
+        for e in pg.event.get():
+            if e.type == pg.QUIT:
+                pg.quit()
+                sys.exit()
+
+        screen.fill((30, 30, 30))
+        game.update()
+
+        pg.display.flip()
+        clock.tick(60)
---
+++ player.py
@@ -0,0 +1,26 @@
+import pygame as pg
+
+
+def change_size(image, multiplier):
+    img_size = image.get_size()
+    return pg.transform.scale(image, (img_size[0] * multiplier, img_size[1] * multiplier))
+
+
+class Player(pg.sprite.Sprite):
+    def __init__(self, pos):
+        super().__init__()
+        self.image = change_size(pg.image.load('src/ship.png').convert_alpha(), 0.1)
+
+        self.rect = self.image.get_rect(midbottom=pos)
+        self.speed = 3
+
+    def get_input(self):
+        keys = pg.key.get_pressed()
+
+        if keys[pg.K_RIGHT]:
+            self.rect.x += self.speed
+        elif keys[pg.K_LEFT]:
+            self.rect.x -= self.speed
+
+    def update(self) -> None:
+        self.get_input()
