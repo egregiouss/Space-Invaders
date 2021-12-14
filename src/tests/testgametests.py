9500f33da8bb0f0b5c121792af5dfbd5b998384a
egregiouss <egorushka645258@gmail.com>
2021-12-14 15:04:19 +0500

    tests

--- src/tests/testgametests.py
+++ src/tests/testgametests.py
@@ -1,24 +1,13 @@
-import pygame as pg
-import pytest
+
+
 import unittest
 
 from src.code.enemy import Enemy
 
 from unittest.mock import patch
 
-
-@pytest.fixture()
-@patch('code.enemy.Enemy.setup_image', lambda x: pg.Surface((50, 50)))
-def enemy():
-    return Enemy((10, 10))
-
-
 class EnemyTests(unittest.TestCase):
     def test_class_instantiate_ok(self):
 
         self.assertIsNotNone("4")
 
-    def test_simple_movement_left(enemy):
-        enemy.move()
-        expected = (10 + 10, 10)
-        assert expected == (enemy.rect.centerx, enemy.rect.centery)
